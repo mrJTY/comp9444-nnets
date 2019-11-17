e6a094bd727492738c03bf83af64f864feefbe55
jtdv01 <jtdv01@gmail.com>
2019-11-17 14:12:17 +1100

    Start part 3

--- hw2/part3.py
+++ hw2/part3.py
@@ -12,24 +12,61 @@ from imdb_dataloader import IMDB
 class Network(tnn.Module):
     def __init__(self):
         super(Network, self).__init__()
+        self.input_dim = 50
+        self.hidden_dim = 100
+        self.num_layers = 3
+        self.batch_size = 64
+        self.lstm_layer = torch.nn.LSTM(input_size=50, hidden_size=self.hidden_dim, batch_first=True,
+                                        num_layers=self.num_layers)
+        self.fc2 = torch.nn.Linear(in_features=self.hidden_dim, out_features=64)
+        self.fc3 = torch.nn.Linear(in_features=64, out_features=1)
 
     def forward(self, input, length):
         """
         DO NOT MODIFY FUNCTION SIGNATURE
         Create the forward pass through the network.
         """
+        batch_size = input.size(0)
+        out, hidden = self.lstm_layer(input, self.zero_hidden(batch_size))
+
+        # TODO: this could be optimised by taking the last hidden output instead
+        fc2_output = self.fc2(out)
+        relu_output = torch.relu(fc2_output)
+
+        fc3_output = self.fc3(relu_output)
+        softmax_output = torch.softmax(fc3_output, dim=1)
+
+        # Get only the last output
+        out = torch.zeros(batch_size)
+        for i in range(0, batch_size):
+            out[i] = softmax_output[i][-1][0]
+
+        # output must just be a single dimension 64 tensor(batch) x 1
+        assert out.shape == torch.Size([batch_size])
+        return out
+
+    def zero_hidden(self, batch_size):
+        return (
+            torch.zeros(self.num_layers, batch_size, self.hidden_dim),
+            torch.zeros(self.num_layers, batch_size, self.hidden_dim)
+        )
 
 
 class PreProcessing():
     def pre(x):
         """Called after tokenization"""
+
+        # x is a list of words
+        # Do some transformations to remove stop words
         return x
 
     def post(batch, vocab):
         """Called after numericalization but prior to vectorization"""
         return batch, vocab
 
-    text_field = data.Field(lower=True, include_lengths=True, batch_first=True, preprocessing=pre, postprocessing=post)
+    # TODO:
+    #text_field = data.Field(lower=True, include_lengths=True, batch_first=True, preprocessing=pre, postprocessing=post)
+    text_field = data.Field(lower=True, include_lengths=True, batch_first=True)
 
 
 def lossFunc():
@@ -37,6 +74,7 @@ def lossFunc():
     Define a loss function appropriate for the above networks that will
     add a sigmoid to the output and calculate the binary cross-entropy.
     """
+    return torch.nn.BCELoss()
 
 def main():
     # Use a GPU if available, as it should be faster.
